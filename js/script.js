bfae32b315514dea1b0ecb3495ccbeed34d565ee
seanniekins <seanaustinj@gmail.com>
2020-09-14 19:28:42 +0800

    Done mobile view

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var myPicture = document.getElementById("my-picture");
+var aboutContent = document.getElementById("about-content");
+var leftArrow = document.getElementById("left-arrow");
+var rightArrow = document.getElementById("right-arrow");
+var currentPage = 0;
+var lastPage = 6;
+
+leftArrow.addEventListener("click", scrollLeft);
+rightArrow.addEventListener("click", scrollRight);
+
+function scrollLeft() {
+    if(currentPage === 0) {
+        currentPage = lastPage;
+    } else {
+        currentPage--;
+    }
+    changePage();
+}
+
+function scrollRight() {
+    if(currentPage === lastPage) {
+        currentPage = 0;
+    } else {
+        currentPage++;
+    }
+    changePage();
+}
+
+function changePage() {
+    fadeOut();
+    setTimeout(changeContent, 500);
+    setTimeout(fadeIn, 500);
+}
+
+function fadeOut() {
+    aboutContent.style.opacity = 0;
+}
+
+function changeContent() {
+    aboutContent.textContent = aboutMe[currentPage]
+}
+
+function fadeIn() {
+    aboutContent.style.opacity = 1;
+}
